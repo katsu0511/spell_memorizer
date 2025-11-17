6e3ef8b69d41922e44828866673a6d2dc26bb2c7
Katsuya <haradakatsuya190511@gmail.com>
2025-11-17 11:42:51 -0800

    new function
    added new function to stop audio

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,13 +1,12 @@
 const views = document.getElementsByClassName('view');
 const viewsArray = Array.from(views);
 viewsArray.forEach(function(view) {
-  view.addEventListener('click', function(){
+	view.addEventListener('click', function(){
 		view.classList.remove('show');
 		view.classList.add('hide');
 		const mask = view.nextElementSibling;
 		mask.classList.remove('hide');
 		mask.classList.add('show');
-		
 		const td = view.parentElement.parentElement;
 		const spellInput = td.querySelector('.spell_input');
 		const word = td.querySelector('.word');
@@ -18,13 +17,12 @@ viewsArray.forEach(function(view) {
 const masks = document.getElementsByClassName('mask');
 const masksArray = Array.from(masks);
 masksArray.forEach(function(mask) {
-  mask.addEventListener('click', function(){
+	mask.addEventListener('click', function(){
 		mask.classList.remove('show');
 		mask.classList.add('hide');
 		const view = mask.previousElementSibling;
 		view.classList.remove('hide');
 		view.classList.add('show');
-		
 		const word = mask.parentElement.parentElement.querySelector('.word');
 		word.textContent = '**********';
 	});
@@ -33,16 +31,54 @@ masksArray.forEach(function(mask) {
 const plays = document.getElementsByClassName('play');
 const playsArray = Array.from(plays);
 let audio = null;
+
+const pauses = document.getElementsByClassName('pause');
+const pausesArray = Array.from(pauses);
+pausesArray.forEach(function(pause) {
+	pause.addEventListener('click', function(){
+		if (audio != null) {
+			pausesArray.forEach(function(p) {
+				p.classList.remove('show');
+				p.classList.add('hide');
+				const replay = p.nextElementSibling;
+				replay.classList.remove('hide');
+				replay.classList.add('show');
+			});
+			audio.pause();
+		}
+	});
+});
+
+const replays = document.getElementsByClassName('replay');
+const replaysArray = Array.from(replays);
+replaysArray.forEach(function(replay) {
+	replay.addEventListener('click', function(){
+		replaysArray.forEach(function(r) {
+			r.classList.remove('show');
+			r.classList.add('hide');
+			const pause = r.previousElementSibling;
+			pause.classList.remove('hide');
+			pause.classList.add('show');
+		});
+		audio.play();
+	});
+});
+
 playsArray.forEach(function(play) {
-  play.addEventListener('click', function(){
+	play.addEventListener('click', function(){
 		const bookName = document.getElementById('book_name');
 		const chapterName = document.getElementById('chapter_name');
 		const mp3FileName = play.nextElementSibling;
-		if (audio != null) {
-			audio.pause();
-		}
+		if (audio != null) audio.pause();
 		audio = new Audio(`../sound/${bookName.textContent}/${chapterName.textContent}/${mp3FileName.value}`);
 		audio.play();
+		replaysArray.forEach(function(replay) {
+			replay.classList.remove('show');
+			replay.classList.add('hide');
+			const pause = replay.previousElementSibling;
+			pause.classList.remove('hide');
+			pause.classList.add('show');
+		});
 	});
 });
 
@@ -51,7 +87,7 @@ const answerButtonsArray = Array.from(answerButtons);
 const correct = new Audio(`../sound/judgement/correct.mp3`);
 const incorrect = new Audio(`../sound/judgement/incorrect.mp3`);
 answerButtonsArray.forEach(function(answerButton) {
-  answerButton.addEventListener('click', function(){
+	answerButton.addEventListener('click', function(){
 		const answer = answerButton.parentElement.parentElement.parentElement.querySelector('.spell_input').value;
 		const myAnswer = answerButton.previousElementSibling.value;
 		const judgement = answerButton.parentElement.parentElement.parentElement.querySelector('.judgement');
@@ -62,11 +98,9 @@ answerButtonsArray.forEach(function(answerButton) {
 		incorrect.pause();
 		incorrect.currentTime = 0;
 		if (answer === myAnswer) {
-			judgement.classList.add('correct');
 			judgement.textContent = '◯';
 			correct.play();
 		} else {
-			judgement.classList.add('incorrect');
 			judgement.textContent = '×';
 			incorrect.play();
 		}
