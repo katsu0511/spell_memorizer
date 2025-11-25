761416f2d87ae7851570f561470904607df08ef4
Katsuya <haradakatsuya190511@gmail.com>
2025-11-25 12:22:32 -0800

    update driver
    update DB driver

--- src/main/java/data/SpellMemorizerDAO.java
+++ src/main/java/data/SpellMemorizerDAO.java
@@ -17,7 +17,7 @@ public class SpellMemorizerDAO {
 		Connection conn = null;
 		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection(DSN, USER, PASSWORD);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
