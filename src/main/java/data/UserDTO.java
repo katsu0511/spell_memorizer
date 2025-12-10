5f3149c9f81d228928cf6f85117b8bc4c6984591
Katsuya <haradakatsuya190511@gmail.com>
2025-12-10 14:14:16 -0800

    created UserDTO

---
+++ src/main/java/data/UserDTO.java
@@ -0,0 +1,22 @@
+package data;
+
+public class UserDTO {
+	private int userId;
+	private String email;
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
